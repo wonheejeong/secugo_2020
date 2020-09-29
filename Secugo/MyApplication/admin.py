7faa1d82ab203a0e7848c89d96ab935759af12c2
wonheejeong <whj6648@gmail.com>
2020-09-30 01:02:34 +0900

    first commit

---
+++ Secugo/MyApplication/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from MyApplication.models import *
+
+# Register your models here.
+admin.site.register(Marchandise)
---
+++ Secugo/MyApplication/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Marchandise(models.Model):
+    name = models.CharField(max_length=50)
+    descript = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    cdate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return [self.id, self.name, self.descript]
